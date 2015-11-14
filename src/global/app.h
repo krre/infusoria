f62e86ea8db0ea1cf47b6d305893a9f698124c87
Vladimir Zarypov <krre31@gmail.com>
2015-11-14 14:15:52 +0200

    added buildDate() function to App class

--- src/global/app.h
+++ src/global/app.h
@@ -20,4 +20,5 @@ public:
     static unsigned int patch() { return PATCH; }
     static QString version() { return QString("%1.%2.%3").arg(MAJOR).arg(MINOR).arg(PATCH); }
     static QString name() { return QString("Infusoria"); }
+    static QString buildDate() { return QString(__DATE__); }
 };
