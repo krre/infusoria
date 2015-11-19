0218a414dc44e95de1cf19a7bc64ccece6a5cf47
Vladimir Zarypov <krre31@gmail.com>
2015-11-19 10:11:38 +0200

    added two-sided data exchange between server and client

--- src/net/websocketmanager.cpp
+++ src/net/websocketmanager.cpp
@@ -7,8 +7,15 @@ WebSocketManager::WebSocketManager()
     connect(server, &QWebSocketServer::acceptError, this, &WebSocketManager::onAcceptError);
     connect(server, &QWebSocketServer::serverError, this, &WebSocketManager::onServerError);
     connect(server, &QWebSocketServer::newConnection, this, &WebSocketManager::onNewConnection);
+    connect(server, &QWebSocketServer::closed, this, &WebSocketManager::closed);
     server->listen();
     console("Infusoria AI unit started. Port " << server->serverPort());
+}
+
+WebSocketManager::~WebSocketManager() {
+    console("WebSocketManager destructor");
+    server->close();
+    qDeleteAll(clients.begin(), clients.end());
 }
 
 void WebSocketManager::onAcceptError(QAbstractSocket::SocketError socketError)
@@ -24,4 +31,36 @@ void WebSocketManager::onServerError(QWebSocketProtocol::CloseCode closeCode)
 void WebSocketManager::onNewConnection()
 {
     console("New connection");
+    QWebSocket *socket = server->nextPendingConnection();
+
+    connect(socket, &QWebSocket::textMessageReceived, this, &WebSocketManager::processTextMessage);
+    connect(socket, &QWebSocket::binaryMessageReceived, this, &WebSocketManager::processBinaryMessage);
+    connect(socket, &QWebSocket::disconnected, this, &WebSocketManager::socketDisconnected);
+
+    clients << socket;
+}
+
+void WebSocketManager::processTextMessage(QString message)
+{
+    QWebSocket *client = qobject_cast<QWebSocket*>(sender());
+    if (client) {
+        client->sendTextMessage(message);
+    }
+}
+
+void WebSocketManager::processBinaryMessage(QByteArray message)
+{
+    QWebSocket *client = qobject_cast<QWebSocket *>(sender());
+    if (client) {
+        client->sendBinaryMessage(message);
+    }
+}
+
+void WebSocketManager::socketDisconnected()
+{
+    QWebSocket *client = qobject_cast<QWebSocket *>(sender());
+    if (client) {
+        clients.removeAll(client);
+        client->deleteLater();
+    }
 }
--- src/net/websocketmanager.h
+++ src/net/websocketmanager.h
@@ -10,14 +10,20 @@ class WebSocketManager : public QObject
     Q_OBJECT
 public:
     WebSocketManager();
-    ~WebSocketManager() { qDebug() << "WebSocketManager destructor"; }
+    ~WebSocketManager();
+
+signals:
+    void closed();
 
 private slots:
     void onAcceptError(QAbstractSocket::SocketError socketError);
     void onServerError(QWebSocketProtocol::CloseCode closeCode);
     void onNewConnection();
+    void processTextMessage(QString message);
+    void processBinaryMessage(QByteArray message);
+    void socketDisconnected();
 
 private:
     QWebSocketServer* server;
-    QWebSocket* client;
+    QList<QWebSocket*> clients;
 };
