3e6d02267eccedf0452598c262c6b997c85af689
Vladimir Zarypov <krre31@gmail.com>
2024-01-19 08:35:22 +0200

    Remove unused properties from App class

--- src/common/App.h
+++ src/common/App.h
@@ -4,9 +4,6 @@
 class App : public QObject {
     Q_OBJECT
     Q_PROPERTY(QString version READ version CONSTANT)
-    Q_PROPERTY(unsigned int major READ major CONSTANT)
-    Q_PROPERTY(unsigned int minor READ minor CONSTANT)
-    Q_PROPERTY(unsigned int patch READ patch CONSTANT)
     Q_PROPERTY(QString buildDate READ buildDate CONSTANT)
     Q_PROPERTY(QString qtVersion READ qtVersion CONSTANT)
     Q_PROPERTY(QString applicationDirPath READ applicationDirPath CONSTANT)
