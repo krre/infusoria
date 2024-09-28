e25ff8ebbb407d0c0ef215808cc593265f5dfc4a
Vladimir Zarypov <krre31@gmail.com>
2024-09-28 09:31:29 +0300

    Remove GTK styling hack

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,6 @@ QPointer<Settings> settings;
 QPointer<InfuController> infuController;
 
 int main(int argc, char* argv[]) {
-    // Hack to fix styling with Qt 6.5 on GTK3
-    qunsetenv("GNOME_DESKTOP_SESSION_ID");
-    qunsetenv("XDG_CURRENT_DESKTOP");
-
     Application app(argc, argv);
     app.setApplicationName("Infusoria");
     app.setOrganizationName("Cyberlife");
